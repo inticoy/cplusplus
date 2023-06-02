b5de0c6c4b3b2ed84eab44c969403991a66717ad
inticoy <inticoy0406@gmail.com>
2023-06-02 17:26:51 +0900

    Feat: module00/ex00

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 14:44:27 by inticoy           #+#    #+#             */
+/*   Updated: 2023/06/02 17:25:49 by gyoon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::toupper;
+
+int main(int argc, char *argv[]) {
+  for (int i = 1; i < argc; i++) {
+    string str = string(argv[i]);
+    for (int i = 0; i < str.length(); i++) str[i] = toupper(str[i]);
+    cout << str << endl;
+  }
+  return 0;
+}
