b87c6cf11eed32116754993d53fd598ec7347b65
gyoon <inticoy0406@gmail.com>
2023-06-03 12:30:49 +0900

    Feat: module00/ex00

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 14:44:27 by inticoy           #+#    #+#             */
-/*   Updated: 2023/06/02 17:26:57 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/03 12:30:14 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,19 @@ using std::cout;
 using std::endl;
 using std::string;
 
-int main(int argc, char *argv[]) {
-  for (int i = 1; i < argc; i++) {
-    string str = string(argv[i]);
-    for (int i = 0; i < str.length(); i++) str[i] = toupper(str[i]);
-    cout << str << endl;
-  }
-  return 0;
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			string str = string(argv[i]);
+			for (int j = 0; j < str.length(); j++) str[j] = toupper(str[j]);
+			cout << str;
+		}
+	}
+	cout << endl;
+	return 0;
 }
