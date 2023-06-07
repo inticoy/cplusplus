4f1ebd469d968a8333c17ec90b05a57119ce2ae6
inticoy <inticoy0406@gmail.com>
2023-06-07 15:37:02 +0900

    Feat: 01/02 Introducing Reference

---
+++ 01/ex02/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/07 14:36:05 by gyoon             #+#    #+#             */
+/*   Updated: 2023/06/07 15:35:59 by gyoon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(void)
+{
+    string brain = "HI THIS IS BRAIN";
+
+    string *stringPTR = &brain;
+    string &stringREF = brain;
+
+    cout << "string address : " << &brain << endl;
+    cout << "*string address : " << stringPTR << endl;
+    cout << "&string address : " << &stringREF << endl;
+
+    cout << "string value : " << brain << endl;
+    cout << "*string value : " << *stringPTR << endl;
+    cout << "&string value : " << stringREF << endl;
+    return (0);
+}
