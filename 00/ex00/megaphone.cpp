5968e6414a9bf80725c082fc7686ecf91e6a106d
inticoy <inticoy0406@gmail.com>
2023-06-08 10:27:33 +0900

    Refactor 00/00 Google conv.

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,31 +6,25 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 14:44:27 by inticoy           #+#    #+#             */
-/*   Updated: 2023/06/03 16:37:34 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/08 10:26:51 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-using std::cout;
-using std::endl;
-using std::string;
-
-int main(int argc, char *argv[])
-{
-    if (argc == 1)
-        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    else
-    {
-        for (int i = 1; i < argc; i++)
-        {
-            string str = string(argv[i]);
-            for (int j = 0; j < str.length(); j++)
-                str[j] = toupper(str[j]);
-            cout << str;
-        }
+int main(int argc, char *argv[]) {
+  if (argc == 1)
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  else {
+    for (int i = 1; i < argc; i++) {
+      std::string str = std::string(argv[i]);
+      for (int j = 0; j < str.length(); j++) {
+        str[j] = toupper(str[j]);
+      }
+      std::cout << str;
     }
-    cout << endl;
-    return 0;
+    std::cout << std::endl;
+  }
+  return 0;
 }
