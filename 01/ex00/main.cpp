d7635e094c774e4e9686de20503fc784df35bc85
inticoy <inticoy0406@gmail.com>
2023-06-13 22:53:06 +0900

    Refactor: 01/00 comments

--- 01/ex00/main.cpp
+++ 01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 14:36:05 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/08 17:03:47 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/13 17:40:19 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 
 int main() {
   std::cout << "[newZombie] Allocate Zombie Dynamically." << std::endl;
-  Zombie *zombie = newZombie("Heap");
-  zombie->announce();
-  delete zombie;
+  Zombie *zombie = newZombie("Heap");  // Dynamic Allocation @ Heap
+  zombie->announce();                  // access by pointer
+  delete zombie;                       // dynamic allocation needs deallocation
+
   std::cout << std::endl;
+
   std::cout << "[randomChump] Using zombie on the stack." << std::endl;
-  randomChump("Stack");
+  randomChump("Stack");  // Static Allocation @ Stack in randomChump
+
   return 0;
 }
