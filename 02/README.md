5fe7d3318e64a5706ef8f96bd43662b62caf5464
inticoy <inticoy0406@gmail.com>
2023-06-15 00:54:25 +0900

    Feat: init 02

---
+++ 02/README.md
@@ -0,0 +1,17 @@
+# CPP Module 02
+
+## Goals
+- Ad-hoc polymorphism
+- operator overloading
+- Orthodox Canonical class form
+
+## ex00: My First Class in Orthodox Canonical Form
+
+
+## ex01: Towards a more useful fixed-point number class
+
+
+## ex02: Now we're talking
+
+
+## ex03: BSP
---
+++ 02/ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 22:49:01 by gyoon             #+#    #+#             */
+/*   Updated: 2023/06/13 22:51:38 by gyoon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "Fixed.hpp"
+
+int main(void) {
+  Fixed a;
+  Fixed b(a);
+  Fixed c;
+
+  c = b;
+
+  std::cout << a.getRawBits() << std::endl;
+  std::cout << b.getRawBits() << std::endl;
+  std::cout << c.getRawBits() << std::endl;
+
+  return 0;
+}
