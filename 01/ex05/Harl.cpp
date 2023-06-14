5fcf19d0251a1f90356a8bf2dfa1c5d0b0140d4a
inticoy <inticoy0406@gmail.com>
2023-06-15 00:23:32 +0900

    Feat: 01/05 Harl

--- 01/ex05/Harl.cpp
+++ 01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 14:14:31 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/11 19:34:55 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/15 00:23:04 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,47 +16,55 @@
 
 const std::string Harl::kLevels[LVLS] = {"debug", "info", "warning", "error",
                                          ""};
+const std::string Harl::kComments[LVLS] = {
+    "I love having extra bacon for my "
+    "7XL-double-chess-triple-pickle-special-ketchup burger.\n"
+    "I really do !",
+    "I cannot believe adding extra bacon costs more money.\n"
+    "You didn't put enough bacon in my burger!\n"
+    "If you did, I wouldn't be asking for more!",
+    "I think I deserve to have some extra bacon for free.\n"
+    "I've been coming for yeears whereas you started working "
+    "here since last month.",
+    "This is unacceptable!\n"
+    "I want to speak to the manager now.",
+    "Undefined level.\n"
+    "Please retry with debug / info / warning / error."};
 
-Harl::Harl() {}
-Harl::~Harl() {}
+Harl::Harl() {
+  fp[kDebug] = &Harl::debug;
+  fp[kInfo] = &Harl::info;
+  fp[kWarning] = &Harl::warning;
+  fp[kError] = &Harl::error;
+  fp[kUndefined] = &Harl::undefined;
+  std::cout << "Harl created." << std::endl;
+}
+Harl::~Harl() { std::cout << "Harl destoryed." << std::endl; }
 
+/* other ways :
+ * void (Harl::*fp[5])() = {...}; or
+ * typedef void (Harl::*fp)();
+ * fp fp_arr[5] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error,
+ *                 &Harl::undefined}; */
 void Harl::complain(std::string level) {
   int lvl_idx = 0;
   size_t found = std::string::npos;
   while (lvl_idx < LVLS && found == std::string::npos) {
     found = level.find(kLevels[lvl_idx++]);
   }
-
-  // typedef void (Harl::*fp)();
-  void (Harl::*fp[5])() = {&Harl::debug, &Harl::info, &Harl::warning,
-                           &Harl::error, &Harl::undefined};
-  // fp fp_arr[5] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error,
-  //                 &Harl::undefined};
   (this->*(fp[--lvl_idx]))();
 }
 
-void Harl::debug() {
-  std::cout << "I love having extra bacon for my ";
-  std::cout << "7XL-double-chess-triple-pickle-special-ketchup burger.";
-  std::cout << " I really do !" << std::endl;
-}
-void Harl::info() {
-  std::cout << "I cannot believe adding extra bacon costs more money. ";
-  std::cout << "You didn't put enough bacon in my burger! ";
-  std::cout << "If you did, I wouldn't be asking for more!" << std::endl;
-}
-void Harl::warning() {
-  std::cout << "I think I deserve to have some extra bacon for free. ";
-  std::cout << "I've been coming for yeears whereas you started working ";
-  std::cout << "here since last month." << std::endl;
-}
-void Harl::error() {
-  std::cout << "This is unacceptable! ";
-  std::cout << "I want to speak to the manager now." << std::endl;
+void Harl::complain(const char *level) {
+  if (!level) {
+    std::cout << kComments[kUndefined] << std::endl;
+  } else {
+    complain(std::string(level));
+  }
 }
 
-void Harl::undefined() {
-  std::cout << "Undefined level. ";
-  std::cout << "Please retry with debug / info / warning / error. ";
-  std::cout << std::endl;
-}
+void Harl::debug() { std::cout << kComments[kDebug] << std::endl; }
+void Harl::info() { std::cout << kComments[kInfo] << std::endl; }
+void Harl::warning() { std::cout << kComments[kWarning] << std::endl; }
+void Harl::error() { std::cout << kComments[kError] << std::endl; }
+void Harl::undefined() { std::cout << kComments[kUndefined] << std::endl; }
--- 01/ex05/main.cpp
+++ 01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 16:56:19 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/11 15:49:27 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/14 23:44:28 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,20 @@
 
 int main() {
   Harl harl;
+  std::cout << color::kYellow << "[DEBUG level]" << color::kEnd << std::endl;
   harl.complain("debug");
+  std::cout << color::kYellow << "[INFO level]" << color::kEnd << std::endl;
   harl.complain("info");
+  std::cout << color::kYellow << "[WARNING level]" << color::kEnd << std::endl;
   harl.complain("warning");
+  std::cout << color::kYellow << "[ERROR level]" << color::kEnd << std::endl;
   harl.complain("error");
+
+  std::cout << color::kRed << "[UNDEFINED level]" << color::kEnd << std::endl;
   harl.complain("");
+  std::cout << color::kRed << "[UNDEFINED level]" << color::kEnd << std::endl;
   harl.complain("undefined");
+  std::cout << color::kRed << "[UNDEFINED level]" << color::kEnd << std::endl;
+  harl.complain(0);
   return 0;
 }
