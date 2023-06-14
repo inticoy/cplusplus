7bc4e96cdec8ca3aec92a62da590b88ccd985a2e
inticoy <inticoy0406@gmail.com>
2023-06-14 21:55:25 +0900

    Feat: 01/01 comment

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 14:36:05 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/08 17:07:22 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/14 21:53:33 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 #include "Zombie.hpp"
 
 int main() {
-  std::cout << "[zombieHorde] Allocate 10 zombies in a single allocation."
-            << std::endl;
-  Zombie *zombie = zombieHorde(10, "zombieHorde");
+  std::cout << "[zombieHorde] Allocate 10 zombies in a single allocation.";
+  std::cout << std::endl;
+  Zombie *zombie = zombieHorde(10, "zombie");
   for (int i = 0; i < 10; i++) {
     zombie[i].announce();
   }
--- 01/ex01/zombieHorde.cpp
+++ 01/ex01/zombieHorde.cpp
@@ -6,14 +6,14 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 15:19:27 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/08 17:10:27 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/14 21:54:27 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie *zombieHorde(int N, std::string name) {
-  Zombie *zombie = new Zombie[N];
+  Zombie *zombie = new Zombie[N];  // allocate zombie array
   for (int i = 0; i < N; i++) {
     zombie[i].set_name(name + std::to_string(i + 1));
   }
