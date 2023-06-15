2fc17be86f457d64f02cf299b12632c794a8c061
inticoy <inticoy0406@gmail.com>
2023-06-15 15:02:26 +0900

    Feat: 02/00

--- 02/ex00/Fixed.cpp
+++ 02/ex00/Fixed.cpp
@@ -6,8 +6,33 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 22:49:30 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/13 22:49:37 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/15 15:00:32 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+#include <iostream>
+
+Fixed::Fixed() : value_(0) {
+  std::cout << "Default constructor called" << std::endl;
+}
+Fixed::Fixed(const Fixed& f) {
+  std::cout << "Copy constructor called" << std::endl;
+  value_ = f.getRawBits();  // *this = f;
+}
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+Fixed& Fixed::operator=(const Fixed& f) {
+  std::cout << "Copy assignment operator called" << std::endl;
+  value_ = f.getRawBits();
+  return *this;
+}
+
+void Fixed::setRawBits(int const raw) {
+  std::cout << "setRawBits member function called" << std::endl;
+  value_ = raw;
+}
+int Fixed::getRawBits(void) const {
+  std::cout << "getRawBits member function called" << std::endl;
+  return value_;
+}
--- 02/ex00/Fixed.hpp
+++ 02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 22:49:40 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/13 22:51:25 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/15 14:24:21 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 
 class Fixed {
 public:
-  Fixed(/* args */);
-  ~Fixed();
+  Fixed();                           // default constructor
+  Fixed(const Fixed& f);             // copy constructor
+  ~Fixed();                          // destructor
+  Fixed& operator=(const Fixed& f);  // overload copy assignment operator
+
+  void setRawBits(int const raw);
+  int getRawBits(void) const;
 
 private:
-  /* data */
+  const static int kFractionalBit = 8;
+  int value_;
 };
 
 #endif
