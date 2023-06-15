7d96cb822c804f02bcd7a54dc8b3b3c9ec22d1cb
inticoy <inticoy0406@gmail.com>
2023-06-15 17:31:17 +0900

    Feat: 02/01

--- 02/ex01/main.cpp
+++ 02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 22:49:01 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/15 17:19:49 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/15 17:30:57 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 int main(void) {
   Fixed a;
   Fixed const b(10);
-  Fixed const c(1.f / 3.f);
+  Fixed const c(42.42f);
   Fixed const d(b);
 
   a = Fixed(1234.4321f);
