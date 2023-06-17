d0fdd0015a19c061b03722bd29304d7a924c4434
gyoon <inticoy0406@gmail.com>
2023-06-17 13:57:13 +0900

    Fix: 01/03 show constructor/destructor msg

--- 01/ex03/HumanA.cpp
+++ 01/ex03/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 16:09:37 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/14 22:07:59 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/17 13:55:20 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,12 @@
 #include <iostream>
 
 HumanA::HumanA(std::string name, Weapon &weapon)
-    : name_(name), weapon_(weapon) {}
-HumanA::~HumanA() {}
+    : name_(name), weapon_(weapon) {
+  std::cout << "HumanA " << name_ << " with weapon created." << std::endl;
+}
+HumanA::~HumanA() {
+  std::cout << "HumanA " << name_ << " destroyed." << std::endl;
+}
 
 void HumanA::attack() {
   std::cout << name_ << " attacks with his/her " << weapon_.get_type();
--- 01/ex03/HumanB.cpp
+++ 01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 16:12:54 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/14 22:08:03 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/17 13:56:44 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 #include <iostream>
 
-HumanB::HumanB(std::string name) : name_(name), weapon_(NULL) {}
-HumanB::~HumanB() {}
+HumanB::HumanB(std::string name) : name_(name), weapon_(NULL) {
+  std::cout << "HumanB " << name_ << " with no weapon created." << std::endl;
+}
+HumanB::~HumanB() {
+  std::cout << "HumanB " << name_ << " destroyed." << std::endl;
+}
 
 void HumanB::attack() {
   std::cout << name_ << " attacks with his/her ";
