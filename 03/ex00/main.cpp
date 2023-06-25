0a034e698a826cc8b0e2621e6d8d2f96e510f122
gyoon <inticoy0406@gmail.com>
2023-06-25 15:01:57 +0900

    Fix: energy point

--- 03/ex00/main.cpp
+++ 03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 21:39:21 by gyoon             #+#    #+#             */
-/*   Updated: 2023/06/23 16:22:21 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/06/25 15:01:43 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,19 @@ int main() {
 
   {
     ClapTrap clap_trap2("inticoy");
-    clap_trap2.attack("gyoon");
-    clap_trap2.takeDamage(5);
+    clap_trap2.attack("0");
+    clap_trap2.attack("1");
+    clap_trap2.attack("2");
+    clap_trap2.attack("3");
+    clap_trap2.attack("4");
+    clap_trap2.attack("5");
+    clap_trap2.attack("6");
+    clap_trap2.attack("7");
+    clap_trap2.attack("8");
+    clap_trap2.attack("9");
+    clap_trap2.takeDamage(10);
     clap_trap2.beRepaired(8);
+    clap_trap2.attack("gyoon");
   }
   return 0;
 }
