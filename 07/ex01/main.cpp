959877005d194df31f85a6822254a78c6b42aadd
inticoy <inticoy0406@gmail.com>
2023-11-25 17:18:08 +0900

    Feat: iter template

---
+++ 07/ex01/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 16:59:01 by gyoon             #+#    #+#             */
+/*   Updated: 2023/11/25 17:17:55 by gyoon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+#include <iostream>
+#include <string>
+
+int main()
+{
+    {
+        std::cout << "TEST 1\n";
+        int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        size_t len = sizeof(arr) / sizeof(int);
+        iter(arr, len, println);
+    }
+
+    {
+        std::cout << "\nTEST 2\n";
+        double arr[] = {42.0, 4.2, 0.42, 0.042};
+        size_t len = sizeof(arr) / sizeof(double);
+        iter(arr, len, println);
+    }
+
+    {
+        std::cout << "\nTEST 3\n";
+        std::string arr[] = {"42", "seoul", ",", "Korea"};
+        size_t len = 4;
+        iter(arr, len, println);
+    }
+    return 0;
+}
