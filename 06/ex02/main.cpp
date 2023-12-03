ff820eb20c37afa85fe1ab76a49188eef9caba44
inticoy <inticoy0406@gmail.com>
2023-12-03 23:02:58 +0900

    Fix: mem leak

--- 06/ex02/main.cpp
+++ 06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 15:23:53 by gyoon             #+#    #+#             */
-/*   Updated: 2023/12/03 22:52:21 by gyoon            ###   ########.fr       */
+/*   Updated: 2023/12/03 23:02:22 by gyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int main()
         Base *bptr = Identifier::generate();
         Identifier::identify(bptr);
         Identifier::identify(*bptr);
+
+        delete bptr;
     }
     return 0;
 }
